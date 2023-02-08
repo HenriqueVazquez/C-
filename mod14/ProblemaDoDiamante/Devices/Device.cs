45c1de35e278a6aded20ab416242b41cbb91a1aa
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-02-08 14:27:49 -0300

    Resolverndo problema diamante com interface

---
+++ mod14/ProblemaDoDiamante/Devices/Device.cs
@@ -0,0 +1,7 @@
+﻿namespace ProblemaDoDiamante.Devices {
+    abstract class Device {
+        public int SerialNumber { get; set; }
+
+        public abstract void ProcessDoc(string document);
+    }
+}
